7834ef1708c08a1612b047b4a776b4aa0a5246ff
satyarajasekhar <ksatya@fnf.com>
2018-12-31 23:38:40 +0530

    test

--- src/test/java/MySeleniumGroupid/MySelenium/AppTest.java
+++ src/test/java/MySeleniumGroupid/MySelenium/AppTest.java
@@ -2,19 +2,41 @@ package MySeleniumGroupid.MySelenium;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Random;
+
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	WebDriver driver = null;
+	String BASE_URL = "http://ec2-35-154-15-249.ap-south-1.compute.amazonaws.com:3001";
+	String userName = "satyarajasekhar"+ (new Random()).nextInt(500);
+	@BeforeClass
+	public void prepareChrome() {
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\rajassat\\Desktop\\career\\Devops\\selenium\\jars"
+				//+ "\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        driver = new ChromeDriver();
+	}
+	@Test
+	public void addNewUser() {
+		driver.get(BASE_URL+"/add.php");
+		driver.findElement(By.name("login")).sendKeys(userName);
+		driver.findElement(By.name("password")).sendKeys(userName);
+		driver.findElement(By.name("click")).click(); System.out.println("Adding user: "+userName+" to the website");
+	}
+	@Test
+	public void loginNewUser() {
+		driver.get(BASE_URL); driver.manage().window().maximize();
+		driver.findElement(By.name("login")).sendKeys(userName);
+		driver.findElement(By.name("password")).sendKeys(userName);
+		driver.findElement(By.name("click")).click();
+	}
 }
